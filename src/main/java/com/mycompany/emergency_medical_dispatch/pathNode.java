fc9ce00683ef72873574e9e9c4901935b960345e
M. Ikhmal Hakimie <24001471@siswa.um.edu.my>
2026-06-01 20:07:04 +0800

    Fixed package names

--- src/main/java/com/mycompany/emergency_medical_dispatch/pathNode.java
+++ src/main/java/com/mycompany/emergency_medical_dispatch/pathNode.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package emergencydispatch;
+
+package com.mycompany.emergency_medical_dispatch;
 
 public class pathNode implements Comparable<pathNode> {
     String location;
